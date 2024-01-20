211d451c12d57b3eeaecb2c07f18cb319c4fc24f
jadc <chehimj@gmail.com>
2024-01-19 19:45:29 -0700

    frontend

---
+++ src/components/Framework.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import Project from './Project.vue'
+
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+    <section>
+        <div class="title">
+            <img src="../assets/logo.svg" alt="Icon">
+            <h2>{{ name }} Projects</h2>
+        </div>
+        <div class="list">
+            <Project name="Project 1"></Project>
+            <Project name="Project 2"></Project>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+section {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 2em;
+
+    background-color: #ccc;
+    border-radius: 0.5em;
+    box-shadow: 0 0.25em 1em 0 rgba(0,0,0,0.5);
+    border: 1px solid #aaa;
+}
+
+.title {
+    display: flex;
+    align-items: center;
+}
+
+img {
+    width: 4em;
+    margin-right: 1em;
+}
+</style>
---
+++ src/components/Project.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+    <article>
+        <p>{{ name }}</p>
+        <input type="checkbox">
+    </article>
+</template>
+
+<style scoped>
+article {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #eee;
+  padding: 1em;
+  margin: 0.5em 0;
+  border-radius: 0.5em;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+input[type=checkbox] {
+  width: 2rem;
+  height: 2rem;
+  vertical-align: middle;
+  -webkit-appearance: none;
+  background: none;
+  border: 0;
+  outline: 0;
+  flex-grow: 0;
+  border-radius: 50%;
+  background-color: #FFFFFF;
+  transition: background 300ms;
+  cursor: pointer;
+}
+
+input[type=checkbox]:checked {
+  background-color: green;
+}
+
+</style>
